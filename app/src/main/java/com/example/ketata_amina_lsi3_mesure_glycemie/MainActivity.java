e3093a0e1126050046f242a0c6651edc382d2b05
Lenovo <amina.ketata00@gmail.com>
2023-10-19 22:26:05 +0100

    int

---
+++ app/src/main/java/com/example/ketata_amina_lsi3_mesure_glycemie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ketata_amina_lsi3_mesure_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ketata_Amina_LSI3_Mesure_Glycemie"
+include(":app")
+ 
